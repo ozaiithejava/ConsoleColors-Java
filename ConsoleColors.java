b276fcea9d75b7761151f1f7494040e09c77f4f9
yigitthejava <82784013+ozaiithejava@users.noreply.github.com>
2023-07-19 01:24:26 +0300

    Create ConsoleColors.java

---
+++ ConsoleColors.java
@@ -0,0 +1,24 @@
+
+public class ConsoleColors {
+    // Konsol renk kodları
+    public static final String RESET = "\033[0m";
+    public static final String BLACK = "\033[0;30m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\033[0;37m";
+    public static final String BOLD_BLACK = "\033[1;30m";
+    public static final String BOLD_RED = "\033[1;31m";
+    public static final String BOLD_GREEN = "\033[1;32m";
+    public static final String BOLD_YELLOW = "\033[1;33m";
+    public static final String BOLD_BLUE = "\033[1;34m";
+    public static final String BOLD_PURPLE = "\033[1;35m";
+    public static final String BOLD_CYAN = "\033[1;36m";
+    public static final String BOLD_WHITE = "\033[1;37m";
+
+    public static String applyColor(String text, String color) {
+        return color + text + RESET;
+    }
